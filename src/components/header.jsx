786cf45f4928e4c3321112f36001a47abf263dda
Ayoubedf <ayoubelouadifi2005@gmail.com>
2024-09-16 19:43:05 +0100

    updated files

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,7 +9,7 @@ const DropDownMenuItem = ({ menuItem }) => (
 		href='#'
 		role='menuitem'
 		tabIndex={0}
-		className='block px-4 py-2 text-sm hover:bg-slate-100 dark:hover:bg-slate-700 hover:text-blue-700 dark:hover:text-blue-500 active:bg-gray-200y'>
+		className='block px-4 py-2 text-sm hover:bg-slate-100 dark:hover:bg-slate-700 hover:text-blue-700 dark:hover:text-blue-500 active:bg-slate-200y'>
 		{menuItem}
 	</a>
 );
@@ -20,13 +20,13 @@ DropDownMenuItem.propTypes = {
 const MenuButton = ({ title, isOpen, toggleMenu }) => (
 	<button
 		type='button'
-		className='inline-flex justify-between py-2 md:px-0 px-3 text-gray-700 dark:text-gray-50 md:w-auto w-full'
+		className='inline-flex justify-between py-2 md:px-0 px-3 text-slate-700 dark:text-slate-50 md:w-auto w-full'
 		aria-expanded={isOpen}
 		aria-haspopup='true'
 		onClick={toggleMenu}>
 		{title}
 		<svg
-			className='-mr-1 h-5 w-5 text-gray-400'
+			className='-mr-1 h-5 w-5 text-slate-400'
 			viewBox='0 0 20 20'
 			fill='currentColor'>
 			<path d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z' />
@@ -66,7 +66,7 @@ const DropDown = ({ menuItems = [], title }) => {
 
 			<div
 				className={clsx(
-					'transform absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white text-slate-700 dark:bg-slate-800 dark:text-white shadow-lg ring-1 ring-black ring-opacity-20 focus:outline-none',
+					'transform absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white text-slate-700 dark:bg-slate-800 dark:text-white shadow-lg dark:ring-1 ring-black ring-opacity-20',
 					{
 						'ease-in duration-150 opacity-100 scale-100': isOpen,
 						'ease-out duration-300 opacity-0 scale-75 pointer-events-none': !isOpen,
@@ -115,7 +115,7 @@ const NavItems = () => {
 			<li>
 				<a
 					href='#'
-					className='text-gray-700 hover:text-blue-700 dark:hover:text-blue-500 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-transparent block px-3 py-2 md:px-0'
+					className='text-slate-700 hover:text-blue-700 dark:hover:text-blue-500 dark:text-slate-50 hover:bg-slate-50 dark:hover:bg-transparent block px-3 py-2 md:px-0'
 					aria-current='false'>
 					Collections
 				</a>
@@ -123,7 +123,7 @@ const NavItems = () => {
 			<li>
 				<a
 					href='#'
-					className='text-gray-700 hover:text-blue-700 dark:hover:text-blue-500 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-transparent block px-3 py-2 md:px-0'
+					className='text-slate-700 hover:text-blue-700 dark:hover:text-blue-500 dark:text-slate-50 hover:bg-slate-50 dark:hover:bg-transparent block px-3 py-2 md:px-0'
 					aria-current='false'>
 					Cart
 				</a>
@@ -150,22 +150,22 @@ const Drawer = ({ isOpen, setIsOpen }) => {
 				ref={drawerRef}
 				id='navbar-drawer'
 				className={clsx(
-					'md:hidden fixed top-0 left-0 z-40 h-screen overflow-y-auto transition-transform bg-white w-80 max-w-[100%] dark:bg-gray-800 shadow',
+					'md:hidden fixed top-0 left-0 z-40 h-screen overflow-y-auto transition-transform bg-white w-80 max-w-[100%] dark:bg-slate-800 shadow',
 					{ '-translate-x-full': !isOpen },
 				)}
 				tabIndex='-1'
 				aria-labelledby='drawer-label'>
-				<div className='drawer-header flex items-center justify-between p-4 border-b border-slate-700'>
+				<div className='drawer-header flex items-center justify-between p-4 border-b dark:border-slate-700 border-slate-100'>
 					<a href='#' id='drawer-label' className='flex'>
 						<Logo width={32} height={32} className='mr-3 dark:text-slate-100' />
-						<span className='self-center text-lg font-semibold whitespace-nowrap dark:text-gray-50'>
+						<span className='self-center text-lg font-semibold whitespace-nowrap dark:text-slate-50'>
 							Marketiqo
 						</span>
 					</a>
 					<button
 						type='button'
 						aria-controls='navbar-drawer'
-						className='md:hidden text-gray-400 dark:hover:bg-gray-700 hover:text-gray-900 dark:hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center p-2'
+						className='md:hidden text-slate-400 dark:hover:bg-slate-700 hover:text-slate-900 dark:hover:text-slate-200 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center p-2'
 						onClick={() => setIsOpen(false)}>
 						<XMarkIcon className='w-6 h-6' />
 						<span className='sr-only'>Close menu</span>
@@ -200,14 +200,14 @@ const Header = () => {
 							<span
 								role='button'
 								aria-label='Account'
-								className='material-symbols-rounded text-4xl p-2 text-gray-600 dark:text-gray-200 fill-current'>
+								className='material-symbols-rounded text-4xl p-2 text-slate-600 dark:text-slate-200'>
 								account_circle
 							</span>
 						</a>
 						<div className='relative flex-1 mx-1'>
 							<div className='absolute inset-y-0 right-0 px-3 flex items-center pointer-events-none'>
 								<svg
-									className='w-5 h-5 text-gray-500'
+									className='w-5 h-5 text-slate-500'
 									fill='currentColor'
 									viewBox='0 0 20 20'
 									xmlns='http://www.w3.org/2000/svg'>
@@ -216,16 +216,16 @@ const Header = () => {
 							</div>
 							<input
 								type='text'
-								className='my-2 p-2 pl-3 text-gray-900 dark:text-white bg-slate-50 dark:bg-slate-900 dark:border-slate-700 bg-gradient-to-b border sm:text-sm focus:ring-blue-500 w-full shadow-sm rounded-3xl'
+								className='my-2 p-2 pl-3 text-slate-900 dark:text-white bg-slate-50 dark:bg-slate-900 dark:border-slate-700 bg-gradient-to-b border sm:text-sm focus:ring-blue-500 w-full shadow-sm rounded-3xl'
 								placeholder='Search product'
 								aria-label='Search products'
 							/>
 						</div>
 					</div>
-					<nav className='flex flex-wrap items-center justify-between border-gray-200 px-2 py-2'>
+					<nav className='flex flex-wrap items-center justify-between border-slate-200 px-2 py-2'>
 						<a href='#' className='flex'>
 							<Logo width={40} height={40} className='mr-3 dark:text-slate-100' />
-							<span className='self-center text-lg font-semibold whitespace-nowrap dark:text-gray-50'>
+							<span className='self-center text-lg font-semibold whitespace-nowrap dark:text-slate-50'>
 								Marketiqo
 							</span>
 						</a>
@@ -233,7 +233,7 @@ const Header = () => {
 							<button
 								onClick={() => setShow(true)}
 								type='button'
-								className='md:hidden text-gray-400 dark:hover:bg-gray-700 hover:text-gray-900 dark:hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center p-2'
+								className='md:hidden text-slate-400 dark:hover:bg-slate-700 hover:text-slate-900 dark:hover:text-slate-200 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center p-2'
 								aria-controls='navbar-drawer'>
 								<span className='sr-only'>Open main menu</span>
 								<Bars3Icon className='w-6 h-6' />
