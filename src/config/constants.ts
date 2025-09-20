7ee4701b9e706e1215cfcc397aff82a62b89dd41
Ayoubedf <ayoubelouadifi2005@gmail.com>
2025-09-20 14:57:40 +0100

    fixes category routes issue.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -33,7 +33,8 @@ export const APP_ROUTES = {
 	PASS_RESET: '/password/reset',
 	PASS_CHANGE: '/password/change',
 
-	CATEGORY: (cat: Category | ':cat') => `/category/${slugify(cat)}`,
+	CATEGORY: (cat: Category | ':cat') =>
+		`/category/${cat === ':cat' ? cat : slugify(cat)}`,
 	COLLECTIONS: (q?: string) => (q ? `/collections/${q}` : '/collections'),
 	STORES: '/stores',
 	STORE_DETAILS: (id: string) => `/stores/${id}`,
